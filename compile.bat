8f319f1d824e70b4e1696708328eb495856fc725
Abdool-Zaid <aztoorabally7447@gmail.com>
2023-10-31 21:05:29 +0200

    'QOL'

--- compile.bat
+++ compile.bat
@@ -1,4 +1,8 @@
-@echo on
+@echo off
+setlocal enabledelayedexpansion
+
+:: Capture the start time
+set "start_time=!time!"
 set "script_path=%~dp0"
 set "engine=%script_path%engine"
 set "main=main"
@@ -14,8 +18,20 @@ set "fin=%script_path%output\%name%.exe"
 copy /b "%love%" + "%out%" "%fin%"
 del /f "%out%"
 
+:: Capture the end time
+set "end_time=!time!"
 
-echo %to_zip%
+:: Calculate the time difference
+set "start_seconds=!start_time:~6,1!"
+set "start_milliseconds=!start_time:~9,2!"
+set "end_seconds=!end_time:~6,1!"
+set "end_milliseconds=!end_time:~9,2!"
 
-@REM exit
- pause
+set /a "elapsed_time_seconds=end_seconds-start_seconds"
+set /a "elapsed_time_milliseconds=end_milliseconds-start_milliseconds"
+
+echo Execution time: !elapsed_time_seconds!.!elapsed_time_milliseconds! seconds
+
+endlocal
+
+pause
