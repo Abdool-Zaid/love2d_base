d7d4a6342547c2245feb38e68d9cefb57a220494
Abdool-Zaid <aztoorabally7447@gmail.com>
2023-11-06 09:27:39 +0200

    cleaning

--- compile.bat
+++ compile.bat
@@ -7,13 +7,14 @@ set "script_path=%~dp0"
 set "engine=%script_path%engine"
 set "main=main"
 set "love=%script_path%engine\love.exe"
+set "zip=C:\Program Files (x86)\7-Zip\7z.exe"
 for %%A in ("%CD%") do set "name=%%~nxA"
 set "to_zip=%script_path%%main%"
 mkdir output
 set "out=%script_path%output\%name%.love"
 set "fin=%script_path%output\%name%.exe"
 
-"C:\Program Files (x86)\7-Zip\7z.exe" a -tzip "%out%" "%to_zip%\*"
+"%zip%" a -tzip "%out%" "%to_zip%\*"
 
 copy /b "%love%" + "%out%" "%fin%"
 del /f "%out%"
@@ -29,7 +30,7 @@ for %%D in (%dll_files%) do (
 set "to_zip=%script_path%output"
 set "out=%script_path%output\export.zip"
 
-"C:\Program Files (x86)\7-Zip\7z.exe" a -tzip "%out%" "%to_zip%\*"
+"%zip%" a -tzip "%out%" "%to_zip%\*"
 
 
 
